499b298920c46999d9218b612defbcf04f91d524
aaeilrv <16-11233@usb.ve>
2025-08-19 11:56:31 -0400

    windows can close now!

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const windows = document.querySelectorAll('.secondary_window')
+const about_window = document.querySelector('#about')
+const contact_window = document.querySelector('#contact')
 
 let z = 1
 
+// open and close windows
+function window_visibility_handling(window, open) {
+  const display = open ? "block" : "none"
+  return window.style.display = display
+}
+
+document.querySelector("#about_button_open").addEventListener("click", () => {
+  window_visibility_handling(about_window, true)
+});
+
+document.querySelector("#contact_button_open").addEventListener("click", () => {
+  window_visibility_handling(contact_window, true)
+});
+
+document.querySelector("#about_close").addEventListener("click", () => {
+  window_visibility_handling(about_window, false)
+});
+
+document.querySelector("#contact_close").addEventListener("click", () => {
+  window_visibility_handling(contact_window, false)
+});
+
+
+// move windows
 windows.forEach((window) => {
 
   const title = window.querySelector('.secondary_navbar')
